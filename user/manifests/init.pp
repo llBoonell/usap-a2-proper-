c731ad56853029ff899c774cfcfceada553b3c73
llBoonell <32806779+llBoonell@users.noreply.github.com>
2017-10-15 15:43:29 +1100

    Created user .pp
    Initial version to ensure repo functionality

---
+++ user/manifests/init.pp
@@ -0,0 +1,19 @@
+#Creates users and sets their parameters
+
+class user
+{
+  user
+  {
+    'becca':
+    ensure => present;
+  }
+  user
+  {
+    'fred':
+    ensure => present;
+  }
+  user
+  {
+    'wilma':
+    ensure => present;
+  }
