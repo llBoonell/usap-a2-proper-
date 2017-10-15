feb69bbb04b0db16d6fe738d594dbf739153bb19
llBoonell <32806779+llBoonell@users.noreply.github.com>
2017-10-15 19:13:51 +1100

    Created hosts init.pp
    Shouldn't need any further revisions.

---
+++ hosts/manifests/init.pp
@@ -0,0 +1,25 @@
+# Host file for RMIT server DNSes (is that a word?)
+
+class hosts
+{
+  host
+  {
+    'jupiter.csit.rmit.edu.au':
+    ip => '131.170.5.135',
+    host_aliases => 'jupiter',
+  }
+  
+  host
+  {
+    'saturn.csit.rmit.edu.au':
+    ip => '131.170.5.132',
+    host_aliases => 'saturn',
+  }
+  
+  host
+  {
+    'titan.csit.rmit.edu.au':
+    ip => '131.170.5.131',
+    host_aliases => 'titan',
+  }
+}
