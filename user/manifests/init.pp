ff6c49938c53c30406825f684eafeba559b4297b
llBoonell <32806779+llBoonell@users.noreply.github.com>
2017-10-15 17:26:13 +1100

    Updated users
    Users now have almost all conditions fulfilled, including SSH key for "Wilma". Also fixed an absent closing brace for the class... -_-
    File now REQUIRES groups and packages to function!!

--- user/manifests/init.pp
+++ user/manifests/init.pp
@@ -2,18 +2,46 @@
 
 class user
 {
+  #requiring groups and packages init.pp files
+  require packages
+  require groups
   user
   {
     'becca':
     ensure => present;
+    home       => '/home/becca',
+    managehome => true,
+    groups     => ['sysadmin', 'cars'],
+    shell      => '/bin/bash',
+    uid        => '10011661',
   }
   user
   {
     'fred':
     ensure => present;
+    home       => '/home/fred',
+    managehome => true,
+    groups     => ['trucks', 'cars', 'wheel'],
+    shell      => '/bin/csh',
+    uid        => '10021661',
   }
   user
   {
     'wilma':
     ensure => present;
+    home           => '/home/wilma',
+    managehome     => true,
+    groups         => ['trucks', 'cars', 'ambulances'],
+    shell          => '/bin/bash',
+    uid            => '10031661',
+    #purging ssh keys... almost missed that
+    purge_ssh_keys => true,
   }
+  # "wilma's" SSH key
+  -> ssh_authorized_key { 'wilmasKey' :
+    ensure => present,
+    user   => 'wilma',
+    type   => 'ssh-rsa',
+    key    => 'WilmasSecretKey',
+  }
+}
