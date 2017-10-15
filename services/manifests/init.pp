c55a0c86e9f329a5118932dd0e4ae30c24367936
llBoonell <32806779+llBoonell@users.noreply.github.com>
2017-10-15 20:30:35 +1100

    Created service init.pp
    Initial revision. Unresolved comment on line 11. 
    
    **Customised 'httpd.conf' still needs to be written**

---
+++ services/manifests/init.pp
@@ -0,0 +1,34 @@
+#Initialisation
+
+class services 
+{
+  file 
+  {
+    '/var/www/s3541661':
+    ensure => 'directory',
+    owner  => 'root',
+    mode   => '0644',
+    #Should be 777? Need to double check
+  }
+  
+  # Replace existing Apache httpd.conf with customised version
+  -> file 
+  { 
+    '/etc/httpd/conf/httpd.conf' :
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => '/etc/puppetlabs/code/environments/production/manifests/configfiles/httpd.conf',
+    require => Package['httpd'],
+  }
+  
+  # Subscription to service
+  -> service 
+  { 
+    'httpd' :
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/httpd/conf/httpd.conf'],
+  }
+}
